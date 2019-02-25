417e8f27b334d40dcd6b79d409c6cdbcd7a4caa7
ambitious-waterfall-9648 <porebadorota2@gmail.com>
2019-02-25 12:51:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (string.toLowerCase) {
-    returns "I can't hear you!"
+    return "I can't hear you!"
   } else if (string.toUpperCase) {
-    returns "YES INDEED!"
+    return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
-    returns "I love you, too."
+    return "I love you, too."
   }
 }
