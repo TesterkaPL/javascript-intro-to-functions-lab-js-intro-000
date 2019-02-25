37dab4c94e6e0d0789c65f13cb9542e648c9a8d0
ambitious-waterfall-9648 <porebadorota2@gmail.com>
2019-02-25 13:10:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function whisper(string){
 }
 
 function logShout(string){
-  console.log('string.toHaveBeenCalledWith')
+  return console.log('string.toHaveBeenCalledWith')
 }
 
 function logWhisper(string){
-  console.log("string.toLowerCase")
+  return console.log("string.toLowerCase")
 }
 
 function sayHiToGrandma(string){
